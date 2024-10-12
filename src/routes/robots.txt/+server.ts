34566e491d6d700dcd9b54f68a5b863f558f1cdf
Phyrone <phyrone@phyrone.de>
2024-10-12 09:53:07 +0200

    ⏪️ Moved robots.txt to static file

--- src/routes/robots.txt/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from './$types';
-import { text } from '@sveltejs/kit';
-
-export const prerender = true;
-
-export const GET: RequestHandler = async () => {
-	return text('User-agent: *\n' + 'Allow: /');
-};
