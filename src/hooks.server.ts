454d8ef4c9ca3a8e6353d892ab5fe09bfd007207
Phyrone <phyrone@phyrone.de>
2024-11-02 18:12:28 +0100

    🐛 Fixed some layout shifts
    Some whitespaces visible in UI got removed by the minifier but brought back during hydration.
    This commit sets the minifier a bit more conservative.
    Also disables the minifier during developement.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,55 +2,46 @@ import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { minify as minify_html } from '@swc/html';
 import { dev } from '$app/environment';
-import { appendFile } from 'fs/promises';
 
 export const minify_html_handler: Handle = async ({ event, resolve }) => {
-	console.log('[Minify Html] Minify:', event.request.url);
 
-	let buffer = '';
-	return resolve(event, {
-		preload(): boolean {
-			return true;
-		},
-		async transformPageChunk({
-			html,
-			done
-		}: {
-			html: string;
-			done: boolean;
-		}): Promise<string | undefined> {
-			buffer += html;
-			if (!done) return '';
-			const { code, errors } = await minify_html(buffer, {
-				removeComments: !dev,
-				collapseWhitespaces: 'all',
-				normalizeAttributes: true,
-				selfClosingVoidElements: true,
-				forceSetHtml5Doctype: true,
-				minifyJs: true,
-				minifyCss: true,
-				minifyJson: true,
-				quotes: false
-			});
-			if (errors && errors.length > 0) {
-				console.error('Error minifying html', errors);
-				if (!dev) {
-					await appendFile(
-						'minify_html_errors.log',
-						'Error minifying html: ' +
-							event.url.pathname +
-							'\n' +
-							'Errors: \n' +
-							errors.map((e) => JSON.stringify(e)).join('\n') +
-							+'\n\n Html: \n' +
-							buffer +
-							'\n\n'
-					);
+
+	if (dev) return resolve(event);
+	else {
+		console.log('[Minify Html] Minify:', event.request.url);
+		let buffer = '';
+		return resolve(event, {
+			preload(): boolean {
+				return true;
+			},
+			async transformPageChunk({
+				html,
+				done
+			}: {
+				html: string;
+				done: boolean;
+			}): Promise<string | undefined> {
+				buffer += html;
+				if (!done) return '';
+				const { code, errors } = await minify_html(buffer, {
+					removeComments: !dev,
+					collapseWhitespaces: 'conservative',
+					normalizeAttributes: true,
+					selfClosingVoidElements: true,
+					forceSetHtml5Doctype: true,
+					minifyJs: true,
+					minifyCss: true,
+					minifyJson: true,
+					quotes: false,
+					filename: event.request.url
+				});
+				if (errors && errors.length > 0) {
+					console.error('Error minifying html', errors);
 					throw new Error('Error minifying html');
-				} else return buffer;
-			} else return code;
-		}
-	});
+				} else return code;
+			}
+		});
+	}
 };
 
 export const handle: Handle = sequence(minify_html_handler);
